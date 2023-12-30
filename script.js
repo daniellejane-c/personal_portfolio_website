92ca595d34a44800e51ddf48fe7720f26898f3f1
Danielle Cheshire <dj.chesh@hotmail.co.uk>
2023-12-30 18:43:35 +0000

    complete website

---
+++ script.js
@@ -0,0 +1,12 @@
+function displayText() {
+    var text1 = document.getElementById('images1')
+    text1.style.display = "block";
+;  }
+  function displayText2() {
+    var text2 = document.getElementById('images2')
+    text2.style.display = "block";
+  }
+  function displayText3() {
+    var text3 = document.getElementById('images3')
+    text3.style.display = "block";
+  }
